526bda1cdfc84135333ab38826569cc43043a355
Eliot <eliot.durand-de-gevigney@etu.cyu.fr>
2024-12-02 18:01:03 +0100

    Update TRIAGE.sh

--- TRIAGE.sh
+++ TRIAGE.sh
@@ -1,2 +1,4 @@
 cut -d ',' -f 1,14 nomfichier = affiche colonne 1 et 14
 cat c-wire_v00.dat | cut -d ';' -f 2,5,6,7,8 | tr '-' '0' affiche colognne 2 5 6 7 8 et remplace - par 0
+
+awk -F';' '$1 != 1' tableau.txt > resultat.txt // garde les lignes dont la colonne 1 n'est pas égale à 1
