8ea3047735ac62ac4254103ff3820f44349212dc
Luxus95 <lucien.byr@gmail.com>
2024-12-09 15:09:58 +0100

    Update TRIAGE.sh

--- TRIAGE.sh
+++ TRIAGE.sh
@@ -3,12 +3,17 @@ cat c-wire_v00.dat | cut -d ';' -f 2,5,6,7,8 | tr '-' '0' affiche colognne 2 5 6
 
 awk -F';' '$1 != 1' tableau.txt > resultat.txt // garde les lignes dont la colonne 1 n'est pas égale à 1
 
+# Vérification du nombre d'arguments
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <chemin_fichier> <type_station> <type_consommateur> [id_centrale]"
+    exit 1
+fi
+
 # Paramètres
 input_file="$1"
 type_station="$2"
 type_consommateur="$3"
 id_centrale="${4:-}"  # Optionnel
-
 
 # Validation des paramètres de type de station
 if [[ "$type_station" != "hvb" && "$type_station" != "hva" && "$type_station" != "lv" ]]; then
@@ -26,4 +31,23 @@ fi
 if { [ "$type_station" = "hvb" ] || [ "$type_station" = "hva" ]; } && { [ "$type_consommateur" = "all" ] || [ "$type_consommateur" = "indiv" ]; }; then
     echo "Erreur : Les options 'hvb all', 'hvb indiv', 'hva all', et 'hva indiv' sont interdites."
     exit 1
+fi
+
+# Vérification de l'exécutable C
+c_executable="ex2"
+c_source="ex2.c"
+
+if [ ! -f "$c_executable" ]; then
+    echo "L'exécutable C n'est pas présent. Compilation en cours..."
+    if [ ! -f "$c_source" ]; then
+        echo "Erreur : Le fichier source $c_source est introuvable."
+        exit 1
+    fi
+
+    gcc -o "$c_executable" "$c_source"
+    if [ $? -ne 0 ]; then
+        echo "Erreur : La compilation a échoué."
+        exit 1
+    fi
+    echo "Compilation réussie."
 fi
