0a19f499153633e9c5279711041b37a6e4df5491
Luxus95 <lucien.byr@gmail.com>
2024-12-17 14:15:38 +0100

    Create utilis.c

---
+++ utilis.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int validateInput(long long id, long long capacity, long long load) {
+  if (id < 0 || capacity < 0 || load < 0) {
+    fprintf(stderr, "Erreur : donnees invalides pour ID %lld.\n", id);
+    return 0;
+  }
+  return 1;
+}
