47691a86876d63831632008945a17633bd2d7580
Luxus95 <lucien.byr@gmail.com>
2024-12-18 12:45:17 +0100

    Update utilis.c

--- codeC/utilis.c
+++ codeC/utilis.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "utils.h"
+#include "utilis.h"
 
 int validateInput(long long id, long long capacity, long long load) {
   if (id < 0 || capacity < 0 || load < 0) {
