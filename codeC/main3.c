143251faddc32f4312ddd7b718f564ac6b46867b
Eliot <eliot.durand-de-gevigney@etu.cyu.fr>
2024-12-20 18:26:22 +0100

    Update main3.c

--- codeC/main3.c
+++ codeC/main3.c
@@ -6,19 +6,19 @@
 int main() {
   int h;
   int *pH = &h;
-  long long id, capacity, load;
+  long long id, capacity, load; // we put the variables in long long because there was an overflow otherwise with c-wire_v00 in input
   Tree *AVLproj = NULL;
 
-  while (scanf("%lld;%lld;%lld\n", &id, &capacity, &load) == 3) {
-    if (validateInput(id, capacity, load)) {
+  while (scanf("%lld;%lld;%lld\n", &id, &capacity, &load) == 3) { // take the data in input in a certain way and check if the form of the data is ok
+    if (validateInput(id, capacity, load)) { // dont work because the minus "-" is tranformed in a 0 in the script shell 
       AVLproj = insertAVL(AVLproj, id, capacity, load, pH);
     } else {
       printf("Donnees non traitees pour ID %lld.\n", id);
     }
   }
 
-  infix(AVLproj);
-  freeAVL(AVLproj);
+  infix(AVLproj); // Print the tree containing the data (the data printed will be redirected in the output file by the script shell)
+  freeAVL(AVLproj); // Free every node of the tree with a recursive function in order to not have memory leak
 
   return 0;
 }
