99d806431066f87ea21e0348f9c2d717e04aa6c5
Luxus95 <lucien.byr@gmail.com>
2024-12-20 15:17:21 +0100

    Update utilis.c

--- codeC/utilis.c
+++ codeC/utilis.c
@@ -3,8 +3,10 @@
 #include "utilis.h"
 
 int validateInput(long long id, long long capacity, long long load) {
-  if (id < 0 || capacity < 0 || load < 0) {
-    fprintf(stderr, "Erreur : donnees invalides pour ID %lld.\n", id);
-    return 0;
-  }
-  return 1;
+  	if (id < 0 || capacity < 0 || load < 0) {
+    		fprintf(stderr, "Erreur : donnees invalides pour ID %lld.\n", id);
+    		return 0;
+  	}
+  	
+	return 1;
+}
