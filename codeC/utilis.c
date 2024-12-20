1171f90920ec34cdd3e8b1370f3629e98d8ee108
Eliot <eliot.durand-de-gevigney@etu.cyu.fr>
2024-12-20 18:27:30 +0100

    Update utilis.c

--- codeC/utilis.c
+++ codeC/utilis.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "utilis.h"
 
+// was supposed to check if the input data were negatives but dont work cause the minus is tranformed into a 0 by the script shell
+
 int validateInput(long long id, long long capacity, long long load) {
   	if (id < 0 || capacity < 0 || load < 0) {
     		fprintf(stderr, "Erreur : donnees invalides pour ID %lld.\n", id);
